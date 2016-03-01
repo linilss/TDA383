b0d547e6697f4469ef5437b4ebef4c4be5033cef
linilss <linusnilsson1@live.se>
2016-03-01 22:10:57 +0100

    commented version of client

--- Labb3.1/client.erl
+++ Labb3.1/client.erl
@@ -2,9 +2,6 @@
 -export([handle/2, initial_state/2]).
 -include_lib("./defs.hrl").
 
-%% inititial_state/2 and handle/2 are used togetger with the genserver module,
-%% explained in the lecture about Generic server.
-
 %% Produce initial state
 initial_state(Nick, GUIName) ->
     #client_st {gui = GUIName,
@@ -12,16 +9,12 @@ initial_state(Nick, GUIName) ->
     			server = not_connected,
     			channel = []}.
 
-%% ---------------------------------------------------------------------------
-
-%% handle/2 handles each kind of request from GUI
-
-%% All requests are processed by handle/2 receiving the request data (and the
-%% current state), performing the needed actions, and returning a tuple
-%% {reply, Reply, NewState}, where Reply is the reply to be sent to the
-%% requesting process and NewState is the new state of the client.
-
 %% Connect to server
+% 	Attempts to connect to a server. If it succeeds, ok is returned and ok, 
+%	NewState is returned to GUI. Otherwise, if the user is already connected
+%	user_already_connected is returned, or if some other error occurs, 
+%	server_not_reached is returned.
+
 handle(St, {connect, Server}) ->
 	NewState = St#client_st{server = Server},
 	ServerAtom = list_to_atom(Server),
@@ -35,7 +28,16 @@ handle(St, {connect, Server}) ->
 	    	{reply, {error, server_not_reached, "Server is not reachable!"}, St}
     end;
 
-%% Disconnect from server
+%% 	Disconnect from server
+
+% 	Uses two case statements to filter out possible errors that might occur 
+%	when trying to disconnect from a server, e g if there is no  server to 
+%	disconnect from, the program returns the atom error, user_not_connected.
+% 	Or if the user still is connected to any channel, client returns the atom
+%	leave_channels_first. Otherwise returns ok,NewState if the disconnect is 
+%	successful, meaning the user has been removed from the server and the 
+%	server in client_st has been set to the atom not_connected.
+
 handle(St, disconnect) ->
 	NewState = St#client_st{server = not_connected},
 	Data = {disconnect, St#client_st.nick, self()},
@@ -57,7 +59,16 @@ handle(St, disconnect) ->
     		{reply, {error, leave_channels_first, "You need to leave all channels first!"}, St}
     end;
 
-% Join channel
+% 	Join channel
+% 	A couple of case statements catches errors such	as if the user is not
+%	connected to any server, not_connected is retrieved from the first case
+%	statement and thus the user can't join any channel and client returns
+%	user_not_connected to GUI. Also, if 'EXIT' is returned from the server
+%	server_not_reached is returned to GUI. Otherwise the client requests
+%	an ok from channel via genserver. If so, NewState is returned to GUI
+% 	with the channel added in client_st. If channel instead returns 
+%	user_already_exists, user_already_joined is returned to GUI.
+
 handle(St, {join, Channel}) ->
 	NewState = St#client_st{channel = [Channel | St#client_st.channel]},
 	ChannelAtom =list_to_atom(Channel),
@@ -81,7 +92,12 @@ handle(St, {join, Channel}) ->
 			end
 	end;			
 
-%% Leave channel
+%% 	Leave channel
+% 	A case statement keeps track of whether the user is connected to the given
+%   channel or not. That is done by requesting an OK from genserver. An atom
+%   user_not_joined is returned if the user is not connected to the given 
+%   channel.
+
 handle(St, {leave, Channel}) ->
     NewState = St#client_st{channel = lists:delete(Channel, St#client_st.channel)},
     ChannelAtom = list_to_atom(Channel),
@@ -94,6 +110,10 @@ handle(St, {leave, Channel}) ->
     end;
 
 % Sending messages
+%	Passes Data to the Channel, through genserver, to be computed. If the user
+% 	is not connected to the server it tries to print in, an atom 
+% 	user_not_joined is returned from channel, otherwise ok.
+
 handle(St, {msg_from_GUI, Channel, Msg}) ->
 	ChannelAtom = list_to_atom(Channel),
 	Data = {send, self(), St#client_st.nick, Msg},
@@ -105,10 +125,17 @@ handle(St, {msg_from_GUI, Channel, Msg}) ->
 	end;
 
 %% Get current nick
+%	Returns the nick of the client's user, stored in the client_st.
+
 handle(St, whoami) ->
     {reply, St#client_st.nick, St} ;
 
 %% Change nick
+%	Attempts to change the nick of the user of the client. Since it's only 
+%	possible to change nick as long as the user isn't connected, the program 
+%	checks if the user is connected to a server. If not, it returns the new
+% 	with the new nick, otherwise returns atom user_already_connected.
+
 handle(St, {nick, Nick}) ->
 	NewState = St#client_st{nick = Nick},
 	case St#client_st.server of
